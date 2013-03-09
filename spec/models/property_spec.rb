a0c55fbaa28b7fc7cb21ce272119e104474bb9c4
Mark McEahern <mark@mceahern.com>
2013-03-09 17:59:02 -0600

    Add tests for Property html parsing methods

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Property do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:property) { build :property }
+
+  describe '#owner_names' do
+    it { property.owner_names.should eql 'HOBBS BILL C' }
+  end
+
+  describe '#reported_owner_address' do
+    it { property.reported_owner_address.should eql "2126 E ANDREWS\nFRESNO CA      -" }
+  end
+
+  describe '#property_type' do
+    it { property.property_type.should eql 'Bank of America Passbooks' }
+  end
+
+  describe '#cash_report' do
+    it { property.cash_report.should eql '$32.94' }
+  end
+
+  describe '#reported_by' do
+    it { property.reported_by.should eql 'Bank of America - Passbook Accounts' }
+  end
 end
