bc65ebba52687cc82d68eb8433ad89ed3a6e6e08
Mark McEahern <mark@mceahern.com>
2013-03-09 20:19:53 -0600

    Run delayed job generator
    Note:  Does not work yet with Rails 4.0
    
    rails generate delayed_job:active_record

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130309152320) do
+ActiveRecord::Schema.define(version: 20130310021816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0
+    t.integer  "attempts",   default: 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "properties", force: true do |t|
     t.integer  "id_number",     null: false
