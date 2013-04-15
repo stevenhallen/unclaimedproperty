9221cbd2be199ad05d9c78d0adbb250e06cca3af
Philip Crawford <philipharry@gmail.com>
2013-04-15 08:50:01 -0500

    about page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,8 @@ class WelcomeController < ApplicationController
     @properties = Property.most_recent(20).where('cash_report > 100')
     @notification = Notification.new
   end
+  
+  def about
+    @notification = Notification.new
+  end
 end
